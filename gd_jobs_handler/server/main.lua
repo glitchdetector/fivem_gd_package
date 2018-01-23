9834cf65d897ba5db3ce7475e0b60ab9bde3a69f
glitchdetector <vegardbendiks@hotmail.com>
2018-01-23 13:09:22 +0100

    Added handler job

---
+++ gd_jobs_handler/server/main.lua
@@ -0,0 +1,58 @@
+RegisterServerEvent("gd_jobs_handler:tryStartJob")
+AddEventHandler("gd_jobs_handler:tryStartJob", function(location, tier)
+     -- Do all the shit about asking if energy is enough etc
+
+    -- IF PLAYER IS NOT HIGH ENOUGH LEVEL FOR TIER
+        -- Show message
+    -- ELSE IF PLAYER DOES NOT HAVE ENERGY
+        -- Show message
+    -- ELSE
+        -- Start job
+        TriggerClientEvent("gd_jobs_handler:startJob", source, location, tier)
+    -- END
+end)
+
+RegisterServerEvent("gd_jobs_handler:finishJob")
+AddEventHandler("gd_jobs_handler:finishJob", function(total_fares, payment, tier)
+     -- Give bonus money based on distance
+    -- distance: length of entire job
+    -- payment: cargo value multiplier
+    -- tier: job tier
+    local pay = (150 * payment) * tier
+    local money = math.floor((total_fares * pay) / 10)
+    TriggerClientEvent("chatMessage", source, "Bonus ^2$" .. money .. " ^0from ^3" .. total_fares .. " fares")
+    -- give xp and whatever
+end)
+
+RegisterServerEvent("gd_jobs_handler:pickupJob")
+AddEventHandler("gd_jobs_handler:pickupJob", function(fares, payment, tier)
+     -- Give money based on distance
+    -- distance: length from previous pickup point
+    -- payment: cargo value multiplier
+    -- tier: job tier
+    local pay = (150 * payment) * tier
+    local money = math.floor(fares * pay)
+    TriggerClientEvent("chatMessage", source, "^0Received ^2$" .. money .. " ^0from ^3" .. fares .. " fares")
+    -- give xp and whatever
+end)
+
+AddEventHandler("chatMessage",function(source,name,msg)
+	if msg:sub(1,4) == "/pos" then
+		TriggerClientEvent("gd:pos",source,msg)
+		CancelEvent()
+	end
+	if msg:sub(1,5) == "/hash" then
+		TriggerClientEvent("gd:hash",source,msg)
+		CancelEvent()
+	end
+end)
+
+RegisterServerEvent("gd:pos")
+AddEventHandler("gd:pos", function(text, pos)
+    local name = text:sub(6)
+    local str = string.format('{name = "%s", x = %f, y = %f, z = %f, h = %f},', name, pos.x, pos.y, pos.z, pos.h)
+    print(str)
+    local file = io.open("markers.dev", "a+")
+    file:write(str .. "\n")
+    file:close()
+end)
