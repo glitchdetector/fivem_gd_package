ce02bc12e45ce079656dd748b095e65806981b57
glitchdetector <vegardbendiks@hotmail.com>
2018-01-27 23:30:30 +0100

    Separated VEHICLES.META (1/2)

--- gd_corp_vehicles/__resource.lua
+++ gd_corp_vehicles/__resource.lua
@@ -1,11 +1,23 @@
 files {
-    'vehicles.meta',
+    'steam/cabs/vehicles.meta',
+    'steam/collinsco/vehicles.meta',
+    'steam/frllc/vehicles.meta',
+    'steam/narwhalcorp/vehicles.meta',
+    'steam/postop/vehicles.meta',
+    'steam/sal/vehicles.meta',
     'carvariations.meta',
     'handling.meta'
 }
 
 data_file 'HANDLING_FILE' 'handling.meta'
-data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+
+data_file 'VEHICLE_METADATA_FILE' 'steam/cabs/vehicles.meta'
+data_file 'VEHICLE_METADATA_FILE' 'steam/collinsco/vehicles.meta'
+data_file 'VEHICLE_METADATA_FILE' 'steam/frllc/vehicles.meta'
+data_file 'VEHICLE_METADATA_FILE' 'steam/narwhalcorp/vehicles.meta'
+data_file 'VEHICLE_METADATA_FILE' 'steam/postop/vehicles.meta'
+data_file 'VEHICLE_METADATA_FILE' 'steam/sal/vehicles.meta'
+
 data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
 
 client_script 'vehicle_names.lua'
